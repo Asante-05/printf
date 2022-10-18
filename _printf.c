449d8f8c5861d68c86b06ac946842144a8d87e1a
RanafaB18 <raziqbaba321@gmail.com>
2022-10-18 13:21:06 +0000

    update

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - prints output according to format
  * @format: string being passed
@@ -6,44 +7,44 @@
  */
 int _printf(char *format, ...)
 {
-    va_list args;
+	va_list args;
 
-    int strlen = _strlen(format);
-    int len = 0;
-    int i;
+	int strlen = _strlen(format);
+	int len = 0;
+	int i;
 
-    va_start(args, format);
-    for (i = 0; i < strlen; i++)
-    {
-        if (format[i] == '%')
-        {
-            switch (format[i + 1])
-            {
-            case 'c':
-                get_char(args);
-                i++;
-                len++;
-                break;
+	va_start(args, format);
+	for (i = 0; i < strlen; i++)
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					get_char(args);
+					i++;
+					len++;
+					break;
 
-            case 's':
-                get_string(args);
-                i++;
-                len++;
-                break;
+				case 's':
+					get_string(args);
+					i++;
+					len++;
+					break;
 
-            case '%':
-                _putchar(37);
-                i++;
-                len++;
-                break;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            len++;
-        }
-    }
-    va_end(args);
-    return len;
+				case '%':
+					_putchar(37);
+					i++;
+					len++;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+	}
+	va_end(args);
+	return (len);
 }
