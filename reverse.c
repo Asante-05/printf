a16119f51f5b7a6a2b67dd5fbe46faa7399466c4
RanafaB18 <raziqbaba321@gmail.com>
2022-10-19 00:58:03 +0000

    update

--- reverse.c
+++ reverse.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- *
- *
+ * reverse - reverse string
+ * @args: list passed
+ * Return: length
  */
 
 int reverse(va_list args)
 {
-    char *string = va_arg(args, char *);
-    int length = _strlen(string);
+	char *string = va_arg(args, char *);
+	int length = _strlen(string);
+	int i;
 
-    int i;
-    if (string == NULL)
-    {
-        return (-1);
-    }
-    for (i = length; i >= 0; i--)
-    {
-        _putchar(string[i]);
-    }
-    return (length);
+	if (string == NULL)
+	{
+		return (-1);
+	}
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(string[i]);
+	}
+	return (length);
 }
